d9533c1d93db37f9c9eea7844453956a5404ed5b
TharLin123 <tharlinhtet97@gmail.com>
2023-10-29 09:13:20 +0800

    init dispensemicroservice

---
+++ dispensemicroservice/src/main/java/com/ncs/nhps/dispensemicroservice/DispensemicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ncs.nhps.dispensemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispensemicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispensemicroserviceApplication.class, args);
+	}
+
+}
